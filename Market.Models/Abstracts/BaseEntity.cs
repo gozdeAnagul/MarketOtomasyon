bb186201034d2694f0109a275f5ce4c34b7fcbb2
Samet Alabey <samet.alabey@hotmail.com>
2019-01-07 14:00:21 +0300

    Kategori tablosu eklendi.

---
+++ Market.Models/Abstracts/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Market.Models.Abstracts
+{
+
+   public class BaseEntity<T>
+    {
+        [Key]
+        [Column(Order = 1)]
+        public T Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
---
+++ Market.Models/Entities/Kategori.cs
@@ -0,0 +1,39 @@
+﻿using Market.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Market.Models.Entities
+{
+    [Table("Kategoriler")]
+    public class Kategori : BaseEntity2<Guid, Guid>
+    {
+        public Kategori()
+        {
+            Id = Guid.NewGuid();
+            Id2 = Guid.NewGuid();
+        }
+
+        [StringLength(50)]
+        [Required]
+        public string KategoriAdi { get; set; }
+
+        [StringLength(50)]
+        public string Aciklama { get; set; }
+
+        public Guid? UstKategoriId { get; set; }
+
+        [Required]
+        public double Kdv { get; set; }
+        
+
+
+        [ForeignKey("UstKategoriId")]
+        public virtual Kategori UstKategori { get; set; }
+
+        public virtual ICollection<Kategori> Kategoriler { get; set; } = new HashSet<Kategori>();
+
+
+    }
+}
