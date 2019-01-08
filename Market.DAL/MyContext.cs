f1afabda0d8013e36c200153c67047af17fd4bc3
Emre Çetin <emre_17_cetin@hotmail.com>
2019-01-08 11:07:25 +0300

    Repositoryler eklendi

--- Market.DAL/MyContext.cs
+++ Market.DAL/MyContext.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 
 namespace Market.DAL
 {
-    class MyContext : DbContext
+    public class MyContext : DbContext
     {
         public MyContext() : base("name=MarketCon")
         {
