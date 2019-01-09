5f3face8a58c131c446f784df7b3573722a64d8d
Gözde Anagül <Section1@WISSEN>
2019-01-09 09:37:00 +0300

    Ekleme Formu ok

--- Market.WFA/CRUD.Designer.cs
+++ Market.WFA/CRUD.Designer.cs
@@ -37,85 +37,281 @@
             this.btnKatEkle = new System.Windows.Forms.Button();
             this.btnUrunEkle = new System.Windows.Forms.Button();
             this.btnUrunDetayEkle = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // lstKategori
             // 
             this.lstKategori.FormattingEnabled = true;
-            this.lstKategori.Location = new System.Drawing.Point(22, 12);
+            this.lstKategori.Location = new System.Drawing.Point(35, 12);
             this.lstKategori.Name = "lstKategori";
-            this.lstKategori.Size = new System.Drawing.Size(198, 277);
+            this.lstKategori.Size = new System.Drawing.Size(198, 147);
             this.lstKategori.TabIndex = 0;
             // 
             // lstUrun
             // 
             this.lstUrun.FormattingEnabled = true;
-            this.lstUrun.Location = new System.Drawing.Point(283, 12);
+            this.lstUrun.Location = new System.Drawing.Point(290, 39);
             this.lstUrun.Name = "lstUrun";
-            this.lstUrun.Size = new System.Drawing.Size(198, 277);
+            this.lstUrun.Size = new System.Drawing.Size(198, 147);
             this.lstUrun.TabIndex = 1;
             // 
             // lstUrunDetay
             // 
             this.lstUrunDetay.FormattingEnabled = true;
-            this.lstUrunDetay.Location = new System.Drawing.Point(544, 12);
+            this.lstUrunDetay.Location = new System.Drawing.Point(544, 39);
             this.lstUrunDetay.Name = "lstUrunDetay";
-            this.lstUrunDetay.Size = new System.Drawing.Size(198, 277);
+            this.lstUrunDetay.Size = new System.Drawing.Size(327, 147);
             this.lstUrunDetay.TabIndex = 2;
             // 
             // txtKategori
             // 
-            this.txtKategori.Location = new System.Drawing.Point(22, 319);
+            this.txtKategori.Location = new System.Drawing.Point(80, 176);
             this.txtKategori.Name = "txtKategori";
-            this.txtKategori.Size = new System.Drawing.Size(198, 20);
+            this.txtKategori.Size = new System.Drawing.Size(153, 20);
             this.txtKategori.TabIndex = 3;
             // 
             // txtUrun
             // 
-            this.txtUrun.Location = new System.Drawing.Point(283, 319);
+            this.txtUrun.Location = new System.Drawing.Point(335, 203);
             this.txtUrun.Name = "txtUrun";
-            this.txtUrun.Size = new System.Drawing.Size(198, 20);
+            this.txtUrun.Size = new System.Drawing.Size(153, 20);
             this.txtUrun.TabIndex = 4;
             // 
             // txtUrunDetay
             // 
-            this.txtUrunDetay.Location = new System.Drawing.Point(544, 319);
+            this.txtUrunDetay.Location = new System.Drawing.Point(589, 203);
             this.txtUrunDetay.Name = "txtUrunDetay";
-            this.txtUrunDetay.Size = new System.Drawing.Size(198, 20);
+            this.txtUrunDetay.Size = new System.Drawing.Size(129, 20);
             this.txtUrunDetay.TabIndex = 5;
             // 
             // btnKatEkle
             // 
-            this.btnKatEkle.Location = new System.Drawing.Point(66, 358);
+            this.btnKatEkle.Location = new System.Drawing.Point(35, 274);
             this.btnKatEkle.Name = "btnKatEkle";
-            this.btnKatEkle.Size = new System.Drawing.Size(97, 42);
+            this.btnKatEkle.Size = new System.Drawing.Size(198, 42);
             this.btnKatEkle.TabIndex = 6;
             this.btnKatEkle.Text = "Ekle";
             this.btnKatEkle.UseVisualStyleBackColor = true;
             // 
             // btnUrunEkle
             // 
-            this.btnUrunEkle.Location = new System.Drawing.Point(334, 358);
+            this.btnUrunEkle.Location = new System.Drawing.Point(290, 238);
             this.btnUrunEkle.Name = "btnUrunEkle";
-            this.btnUrunEkle.Size = new System.Drawing.Size(97, 42);
+            this.btnUrunEkle.Size = new System.Drawing.Size(198, 42);
             this.btnUrunEkle.TabIndex = 7;
             this.btnUrunEkle.Text = "Ekle";
             this.btnUrunEkle.UseVisualStyleBackColor = true;
             // 
             // btnUrunDetayEkle
             // 
-            this.btnUrunDetayEkle.Location = new System.Drawing.Point(602, 358);
+            this.btnUrunDetayEkle.Location = new System.Drawing.Point(673, 332);
             this.btnUrunDetayEkle.Name = "btnUrunDetayEkle";
-            this.btnUrunDetayEkle.Size = new System.Drawing.Size(97, 42);
+            this.btnUrunDetayEkle.Size = new System.Drawing.Size(198, 42);
             this.btnUrunDetayEkle.TabIndex = 8;
             this.btnUrunDetayEkle.Text = "Ekle";
             this.btnUrunDetayEkle.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 210);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(155, 48);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(290, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(673, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(198, 21);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(589, 234);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(129, 56);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(607, 296);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(111, 20);
+            this.numericUpDown1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Kategori";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Ürün";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(541, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Detay";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(533, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Açıklama";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Açıklama";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(801, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(801, 292);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(735, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Alış Fiyatı";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(735, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Satış Fiyatı";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(801, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 20);
+            this.textBox3.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(735, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Koli İçiAdet";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(735, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(57, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Ürün Adeti";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(801, 199);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(70, 20);
+            this.textBox4.TabIndex = 26;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(541, 303);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 13);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Koli Adet";
             // 
             // CRUD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 433);
+            this.ClientSize = new System.Drawing.Size(898, 424);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnUrunDetayEkle);
             this.Controls.Add(this.btnUrunEkle);
             this.Controls.Add(this.btnKatEkle);
@@ -127,6 +323,7 @@
             this.Controls.Add(this.lstKategori);
             this.Name = "CRUD";
             this.Text = "CRUD";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,5 +340,24 @@
         private System.Windows.Forms.Button btnKatEkle;
         private System.Windows.Forms.Button btnUrunEkle;
         private System.Windows.Forms.Button btnUrunDetayEkle;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label10;
     }
 }
