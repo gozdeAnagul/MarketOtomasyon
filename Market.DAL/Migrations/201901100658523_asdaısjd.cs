ee77a242e03e3dc4a8b6924fb01a6945c2cbcef8
Samet Alabey <samet.alabey@hotmail.com>
2019-01-10 10:16:44 +0300

    update

--- Market.DAL/Migrations/201901100658523_asdaısjd.cs
+++ Market.DAL/Migrations/201901100658523_asdaısjd.cs
@@ -6,12 +6,12 @@ namespace Market.DAL.Migrations
     {
         public override void Up()
         {
-            AlterColumn("dbo.Urunler", "Kdv", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            
         }
         
         public override void Down()
         {
-            AlterColumn("dbo.Urunler", "Kdv", c => c.Double(nullable: false));
+            
         }
     }
 }
