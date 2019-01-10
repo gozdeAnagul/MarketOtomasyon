db2614e6bfa381817ba55f0c1191ce8079830728
Samet Alabey <samet.alabey@hotmail.com>
2019-01-10 12:04:09 +0300

    Mal Kabul eklendi.

--- Market.WFA/MalKabul.Designer.cs
+++ Market.WFA/MalKabul.Designer.cs
@@ -28,28 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAra = new System.Windows.Forms.Button();
             this.btnStogaEkle = new System.Windows.Forms.Button();
-            this.nudKutu = new System.Windows.Forms.NumericUpDown();
+            this.nuKutu = new System.Windows.Forms.NumericUpDown();
             this.label9 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txtBarkod = new System.Windows.Forms.TextBox();
             this.pbBarkod = new System.Windows.Forms.PictureBox();
-            this.btnBarkodBul = new System.Windows.Forms.Button();
             this.cmbKategoriler = new System.Windows.Forms.ComboBox();
             this.lstUrunler = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.nudKutu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuKutu)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBarkod)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnAra
-            // 
-            this.btnAra.Location = new System.Drawing.Point(364, 349);
-            this.btnAra.Name = "btnAra";
-            this.btnAra.Size = new System.Drawing.Size(84, 32);
-            this.btnAra.TabIndex = 44;
-            this.btnAra.Text = "Ara";
-            this.btnAra.UseVisualStyleBackColor = true;
             // 
             // btnStogaEkle
             // 
@@ -59,13 +48,14 @@
             this.btnStogaEkle.TabIndex = 43;
             this.btnStogaEkle.Text = "Stoğa Ekle";
             this.btnStogaEkle.UseVisualStyleBackColor = true;
+            this.btnStogaEkle.Click += new System.EventHandler(this.btnStogaEkle_Click);
             // 
-            // nudKutu
+            // nuKutu
             // 
-            this.nudKutu.Location = new System.Drawing.Point(421, 195);
-            this.nudKutu.Name = "nudKutu";
-            this.nudKutu.Size = new System.Drawing.Size(148, 20);
-            this.nudKutu.TabIndex = 41;
+            this.nuKutu.Location = new System.Drawing.Point(421, 195);
+            this.nuKutu.Name = "nuKutu";
+            this.nuKutu.Size = new System.Drawing.Size(148, 20);
+            this.nuKutu.TabIndex = 41;
             // 
             // label9
             // 
@@ -89,7 +79,7 @@
             // 
             this.txtBarkod.Location = new System.Drawing.Point(354, 52);
             this.txtBarkod.Name = "txtBarkod";
-            this.txtBarkod.Size = new System.Drawing.Size(165, 20);
+            this.txtBarkod.Size = new System.Drawing.Size(215, 20);
             this.txtBarkod.TabIndex = 37;
             // 
             // pbBarkod
@@ -99,15 +89,6 @@
             this.pbBarkod.Size = new System.Drawing.Size(215, 111);
             this.pbBarkod.TabIndex = 56;
             this.pbBarkod.TabStop = false;
-            // 
-            // btnBarkodBul
-            // 
-            this.btnBarkodBul.Location = new System.Drawing.Point(525, 50);
-            this.btnBarkodBul.Name = "btnBarkodBul";
-            this.btnBarkodBul.Size = new System.Drawing.Size(44, 23);
-            this.btnBarkodBul.TabIndex = 57;
-            this.btnBarkodBul.Text = "Bul";
-            this.btnBarkodBul.UseVisualStyleBackColor = true;
             // 
             // cmbKategoriler
             // 
@@ -134,18 +115,16 @@
             this.ClientSize = new System.Drawing.Size(594, 422);
             this.Controls.Add(this.lstUrunler);
             this.Controls.Add(this.cmbKategoriler);
-            this.Controls.Add(this.btnBarkodBul);
             this.Controls.Add(this.pbBarkod);
-            this.Controls.Add(this.btnAra);
             this.Controls.Add(this.btnStogaEkle);
-            this.Controls.Add(this.nudKutu);
+            this.Controls.Add(this.nuKutu);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtBarkod);
             this.Name = "MalKabul";
             this.Text = "MalKabul";
             this.Load += new System.EventHandler(this.MalKabul_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.nudKutu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuKutu)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBarkod)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -153,14 +132,12 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnAra;
         private System.Windows.Forms.Button btnStogaEkle;
-        private System.Windows.Forms.NumericUpDown nudKutu;
+        private System.Windows.Forms.NumericUpDown nuKutu;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtBarkod;
         private System.Windows.Forms.PictureBox pbBarkod;
-        private System.Windows.Forms.Button btnBarkodBul;
         private System.Windows.Forms.ComboBox cmbKategoriler;
         private System.Windows.Forms.ListBox lstUrunler;
     }
