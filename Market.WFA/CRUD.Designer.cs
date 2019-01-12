e782c50835e54fc422fdcfa69d96cd91e747cb59
gözde anagül <gozde.anagul@outlook.com>
2019-01-12 16:10:17 +0300

    null referans hatası düzeltildi

--- Market.WFA/CRUD.Designer.cs
+++ Market.WFA/CRUD.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lstKategori = new System.Windows.Forms.ListBox();
             this.lstUrunler = new System.Windows.Forms.ListBox();
             this.txtKategori = new System.Windows.Forms.TextBox();
@@ -47,194 +48,220 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txtUrunAdi = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.cmsKatSil = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kategoriSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsUrunSil = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.urunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsKatSil.SuspendLayout();
+            this.cmsUrunSil.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstKategori
             // 
+            this.lstKategori.ContextMenuStrip = this.cmsKatSil;
             this.lstKategori.FormattingEnabled = true;
-            this.lstKategori.ItemHeight = 16;
-            this.lstKategori.Location = new System.Drawing.Point(47, 15);
-            this.lstKategori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstKategori.Location = new System.Drawing.Point(35, 12);
             this.lstKategori.Name = "lstKategori";
-            this.lstKategori.Size = new System.Drawing.Size(417, 180);
+            this.lstKategori.Size = new System.Drawing.Size(314, 147);
             this.lstKategori.TabIndex = 0;
             // 
             // lstUrunler
             // 
+            this.lstUrunler.ContextMenuStrip = this.cmsUrunSil;
             this.lstUrunler.FormattingEnabled = true;
-            this.lstUrunler.ItemHeight = 16;
-            this.lstUrunler.Location = new System.Drawing.Point(595, 55);
-            this.lstUrunler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstUrunler.Location = new System.Drawing.Point(446, 45);
             this.lstUrunler.Name = "lstUrunler";
-            this.lstUrunler.Size = new System.Drawing.Size(507, 180);
+            this.lstUrunler.Size = new System.Drawing.Size(381, 147);
             this.lstUrunler.TabIndex = 2;
             // 
             // txtKategori
             // 
-            this.txtKategori.Location = new System.Drawing.Point(261, 270);
-            this.txtKategori.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKategori.Location = new System.Drawing.Point(194, 166);
             this.txtKategori.Name = "txtKategori";
-            this.txtKategori.Size = new System.Drawing.Size(203, 22);
+            this.txtKategori.Size = new System.Drawing.Size(153, 20);
             this.txtKategori.TabIndex = 3;
             // 
             // btnKatEkle
             // 
-            this.btnKatEkle.Location = new System.Drawing.Point(201, 390);
-            this.btnKatEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKatEkle.Location = new System.Drawing.Point(151, 258);
             this.btnKatEkle.Name = "btnKatEkle";
-            this.btnKatEkle.Size = new System.Drawing.Size(264, 52);
+            this.btnKatEkle.Size = new System.Drawing.Size(198, 42);
             this.btnKatEkle.TabIndex = 6;
-            this.btnKatEkle.Text = "Ekle";
+            this.btnKatEkle.Text = "Kategori Ekle";
             this.btnKatEkle.UseVisualStyleBackColor = true;
             this.btnKatEkle.Click += new System.EventHandler(this.btnKatEkle_Click);
             // 
             // btnUrunEkle
             // 
-            this.btnUrunEkle.Location = new System.Drawing.Point(691, 401);
-            this.btnUrunEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUrunEkle.Location = new System.Drawing.Point(672, 326);
             this.btnUrunEkle.Name = "btnUrunEkle";
-            this.btnUrunEkle.Size = new System.Drawing.Size(412, 52);
+            this.btnUrunEkle.Size = new System.Drawing.Size(155, 42);
             this.btnUrunEkle.TabIndex = 7;
-            this.btnUrunEkle.Text = "Ekle";
+            this.btnUrunEkle.Text = "Ürün Ekle";
             this.btnUrunEkle.UseVisualStyleBackColor = true;
             this.btnUrunEkle.Click += new System.EventHandler(this.btnUrunEkle_Click);
             // 
             // txtKategoriAciklama
             // 
-            this.txtKategoriAciklama.Location = new System.Drawing.Point(261, 311);
-            this.txtKategoriAciklama.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKategoriAciklama.Location = new System.Drawing.Point(194, 200);
             this.txtKategoriAciklama.Name = "txtKategoriAciklama";
-            this.txtKategoriAciklama.Size = new System.Drawing.Size(205, 58);
+            this.txtKategoriAciklama.Size = new System.Drawing.Size(155, 48);
             this.txtKategoriAciklama.TabIndex = 9;
             this.txtKategoriAciklama.Text = "";
             // 
             // cmbKategoriler
             // 
             this.cmbKategoriler.FormattingEnabled = true;
-            this.cmbKategoriler.Location = new System.Drawing.Point(595, 22);
-            this.cmbKategoriler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbKategoriler.Location = new System.Drawing.Point(446, 18);
             this.cmbKategoriler.Name = "cmbKategoriler";
-            this.cmbKategoriler.Size = new System.Drawing.Size(507, 24);
+            this.cmbKategoriler.Size = new System.Drawing.Size(381, 21);
             this.cmbKategoriler.TabIndex = 12;
             this.cmbKategoriler.Text = "Kategori Seciniz";
             this.cmbKategoriler.SelectedIndexChanged += new System.EventHandler(this.cmbKategoriler_SelectedIndexChanged_1);
             // 
             // txtUrunAciklama
             // 
-            this.txtUrunAciklama.Location = new System.Drawing.Point(691, 300);
-            this.txtUrunAciklama.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUrunAciklama.Location = new System.Drawing.Point(518, 244);
             this.txtUrunAciklama.Name = "txtUrunAciklama";
-            this.txtUrunAciklama.Size = new System.Drawing.Size(203, 68);
+            this.txtUrunAciklama.Size = new System.Drawing.Size(153, 56);
             this.txtUrunAciklama.TabIndex = 13;
             this.txtUrunAciklama.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(192, 273);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(142, 169);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "Kategori";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(612, 304);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(459, 247);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 18;
             this.label4.Text = "Açıklama:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(187, 315);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(138, 203);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 19;
             this.label5.Text = "Açıklama";
             // 
             // txtAlisFiyati
             // 
-            this.txtAlisFiyati.Location = new System.Drawing.Point(1009, 266);
-            this.txtAlisFiyati.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAlisFiyati.Location = new System.Drawing.Point(757, 216);
             this.txtAlisFiyati.Name = "txtAlisFiyati";
-            this.txtAlisFiyati.Size = new System.Drawing.Size(92, 22);
+            this.txtAlisFiyati.Size = new System.Drawing.Size(70, 20);
             this.txtAlisFiyati.TabIndex = 20;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(935, 270);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(701, 219);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(71, 17);
+            this.label6.Size = new System.Drawing.Size(53, 13);
             this.label6.TabIndex = 22;
             this.label6.Text = "Alış Fiyatı:";
             // 
             // txtKoliIciAdet
             // 
-            this.txtKoliIciAdet.Location = new System.Drawing.Point(1009, 336);
-            this.txtKoliIciAdet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKoliIciAdet.Location = new System.Drawing.Point(757, 273);
             this.txtKoliIciAdet.Name = "txtKoliIciAdet";
-            this.txtKoliIciAdet.Size = new System.Drawing.Size(92, 22);
+            this.txtKoliIciAdet.Size = new System.Drawing.Size(70, 20);
             this.txtKoliIciAdet.TabIndex = 24;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(917, 340);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(688, 276);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(81, 17);
+            this.label8.Size = new System.Drawing.Size(63, 13);
             this.label8.TabIndex = 25;
             this.label8.Text = "Koli İçiAdet:";
             // 
             // txtKdv
             // 
-            this.txtKdv.Location = new System.Drawing.Point(1009, 304);
-            this.txtKdv.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtKdv.Location = new System.Drawing.Point(757, 247);
             this.txtKdv.Name = "txtKdv";
-            this.txtKdv.Size = new System.Drawing.Size(92, 22);
+            this.txtKdv.Size = new System.Drawing.Size(70, 20);
             this.txtKdv.TabIndex = 21;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(967, 308);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(725, 250);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.Size = new System.Drawing.Size(29, 13);
             this.label3.TabIndex = 23;
             this.label3.Text = "Kdv:";
             // 
             // txtUrunAdi
             // 
-            this.txtUrunAdi.Location = new System.Drawing.Point(691, 262);
-            this.txtUrunAdi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUrunAdi.Location = new System.Drawing.Point(518, 213);
             this.txtUrunAdi.Name = "txtUrunAdi";
-            this.txtUrunAdi.Size = new System.Drawing.Size(203, 22);
+            this.txtUrunAdi.Size = new System.Drawing.Size(153, 20);
             this.txtUrunAdi.TabIndex = 4;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(615, 266);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(461, 216);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(67, 17);
+            this.label11.Size = new System.Drawing.Size(51, 13);
             this.label11.TabIndex = 16;
             this.label11.Text = "Ürün Adı:";
             // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(464, 326);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(155, 42);
+            this.btnGuncelle.TabIndex = 27;
+            this.btnGuncelle.Text = "Ürün Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // cmsKatSil
+            // 
+            this.cmsKatSil.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kategoriSilToolStripMenuItem});
+            this.cmsKatSil.Name = "cmsKatSil";
+            this.cmsKatSil.Size = new System.Drawing.Size(136, 26);
+            // 
+            // kategoriSilToolStripMenuItem
+            // 
+            this.kategoriSilToolStripMenuItem.Name = "kategoriSilToolStripMenuItem";
+            this.kategoriSilToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.kategoriSilToolStripMenuItem.Text = "Kategori_Sil";
+            this.kategoriSilToolStripMenuItem.Click += new System.EventHandler(this.kategoriSilToolStripMenuItem_Click);
+            // 
+            // cmsUrunSil
+            // 
+            this.cmsUrunSil.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.urunToolStripMenuItem});
+            this.cmsUrunSil.Name = "cmsUrunSil";
+            this.cmsUrunSil.Size = new System.Drawing.Size(118, 26);
+            // 
+            // urunToolStripMenuItem
+            // 
+            this.urunToolStripMenuItem.Name = "urunToolStripMenuItem";
+            this.urunToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.urunToolStripMenuItem.Text = "Urun_Sil";
+            this.urunToolStripMenuItem.Click += new System.EventHandler(this.urunToolStripMenuItem_Click);
+            // 
             // CRUD
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1224, 577);
+            this.ClientSize = new System.Drawing.Size(918, 469);
+            this.Controls.Add(this.btnGuncelle);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.txtKoliIciAdet);
             this.Controls.Add(this.label3);
@@ -254,10 +281,11 @@
             this.Controls.Add(this.txtKategori);
             this.Controls.Add(this.lstUrunler);
             this.Controls.Add(this.lstKategori);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CRUD";
             this.Text = "CRUD";
             this.Load += new System.EventHandler(this.CRUD_Load);
+            this.cmsKatSil.ResumeLayout(false);
+            this.cmsUrunSil.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -284,5 +312,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtUrunAdi;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.ContextMenuStrip cmsKatSil;
+        private System.Windows.Forms.ToolStripMenuItem kategoriSilToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip cmsUrunSil;
+        private System.Windows.Forms.ToolStripMenuItem urunToolStripMenuItem;
     }
 }
--- Market.WFA/CRUD.cs
+++ Market.WFA/CRUD.cs
@@ -1,6 +1,8 @@
 ﻿using Market.BLL.Repository;
+using Market.DAL;
 using Market.Models.Entities;
 using System;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -13,6 +15,8 @@ namespace Market.WFA
             InitializeComponent();
         }
         Kategori seciliKategori;
+        Kategori lstSeciliKat;
+        Urun lstSeciliUrun;
         private void CRUD_Load(object sender, EventArgs e)
         {
             VerileriDoldur();
@@ -23,8 +27,6 @@ namespace Market.WFA
         {
 
             seciliKategori = cmbKategoriler.SelectedItem as Kategori;
-
-
             lstUrunler.DataSource = null;
             lstKategori.DataSource = new KategoriRepo().GetAll();
             cmbKategoriler.DataSource = new KategoriRepo().GetAll();
@@ -59,9 +61,7 @@ namespace Market.WFA
 
         private void cmbKategoriler_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-           
             seciliKategori = cmbKategoriler.SelectedItem as Kategori;
-
             if (seciliKategori == null) return;
 
             var urunler = new UrunRepo().GetAll(x => x.KategoriId == seciliKategori.Id);
@@ -72,7 +72,7 @@ namespace Market.WFA
             if (seciliKategori == null) return;
 
             try
-            {            
+            {
                 Random rnd = new Random();
 
                 var urun = new Urun
@@ -102,7 +102,58 @@ namespace Market.WFA
                 MessageBox.Show(ex.Message);
             }
         }
+ 
+        private void kategoriSilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lstSeciliKat = lstKategori.SelectedItem as Kategori;
+            if (lstSeciliKat == null) return;
 
-      
+            var cevap = MessageBox.Show("Secili Kategoriyi silmek istiyor musunuz?", "Kategori silme",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (cevap != DialogResult.Yes) return;
+            try
+            {
+                var silinecekKat = new KategoriRepo();
+                silinecekKat.Delete(lstSeciliKat);
+               
+                VerileriDoldur();
+            }
+            catch (DbUpdateException ex)
+            {
+                MessageBox.Show("Silmek istediginiz kayit baska bir tabloda kullanildigi icin silemezsiniz");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void urunToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lstSeciliUrun = lstUrunler.SelectedItem as Urun;
+            if (lstUrunler.SelectedItem == null) return;
+
+            var cevap = MessageBox.Show("Secili Ürünü silmek istiyor musunuz?", "Urun silme",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (cevap != DialogResult.Yes) return;
+            try
+            {
+                var silinecekUrun = new UrunRepo();
+                silinecekUrun.Delete(lstSeciliUrun);
+                VerileriDoldur();
+                
+
+            }
+            catch (DbUpdateException ex)
+            {
+                MessageBox.Show("Silmek istediginiz kayit baska bir tabloda kullanildigi icin silemezsiniz");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
