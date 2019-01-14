909719ba01a519d851f5d5d7c4b1ba62e3fd0784
Samet Alabey <samet.alabey@hotmail.com>
2019-01-14 21:54:15 +0300

    vgfh

--- Market.BLL/MockData/HazirVeri.cs
+++ Market.BLL/MockData/HazirVeri.cs
@@ -1,10 +1,7 @@
 ﻿using Market.BLL.Repository;
 using Market.Models.Entities;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Market.BLL.HazırVeri
 {
